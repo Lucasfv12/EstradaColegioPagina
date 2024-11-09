bf65c8eaa17158a1c881de7d8fa371bc6f6abeea
Lucas <lucasvergara.f@gmail.com>
2024-11-09 20:01:09 -0300

    cambiando a css

---
+++ Desktop/PaginaColegioEstrada-main/src/Components/LevelBoxes.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  FaGraduationCap,
+  FaChurch,
+  FaBook,
+  FaPaintBrush,
+} from "react-icons/fa";
+import "../styles/LevelBoxes.css";
+
+const LevelBoxes = () => {
+  return (
+    <div className="level-boxes">
+      <div className="box box-color1">
+        <FaPaintBrush className="logo" />
+        <a className="titulo-box">NIVEL INICIAL</a>
+      </div>
+      <div className="box box-color2">
+        <FaBook className="logo" />
+        <a className="titulo-box">NIVEL PRIMARIO</a>
+      </div>
+      <div className="box box-color3">
+        <FaGraduationCap className="logo" />
+        <a className="titulo-box">NIVEL SECUNDARIO</a>
+      </div>
+      <div className="box box-color4">
+        <FaChurch className="logo" />
+        <a className="titulo-box">PASTORAL</a>
+      </div>
+    </div>
+  );
+};
+
+export default LevelBoxes;
---
+++ Desktop/PaginaColegioEstrada-main/src/Components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "../styles/NavBar.css";
+import logo from "../assets/Escudosinfondo.png"; // Asegúrate de que la ruta sea correcta
+
+const NavBar = ({ className }) => {
+  return (
+    <nav className={`navbar ${className}`}>
+      <div className="navbar-left">
+        {className === "scrolled" && (
+          <>
+            <img src={logo} alt="Instituto Logo" className="navbar-logo" />
+            <span className="navbar-text">Instituto José Manuel Estrada</span>
+          </>
+        )}
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <a href="#institucional">Institucional</a>
+        </li>
+        <li>
+          <a href="#administracion">Administración</a>
+        </li>
+        <li>
+          <a href="#niveles-educativos">Niveles Educativos</a>
+        </li>
+        <li>
+          <a href="#pastoral">Pastoral</a>
+        </li>
+        <li>
+          <a href="#contactos">Contactos</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
