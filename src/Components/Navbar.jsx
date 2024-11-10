9b561c03e83f4fee4218a7ae597cf8144df34598
Lucas <lucasvergara.f@gmail.com>
2024-11-10 16:27:49 -0300

    cambiando ruta navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/NavBar.css";
+import "../styles/Navbar.css";
 import logo from "../assets/Escudosinfondo.png"; // Asegúrate de que la ruta sea correcta
 
 const NavBar = ({ className }) => {
